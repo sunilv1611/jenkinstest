b35f609b2c8dfa87848496bd7b3efd3c979ba6ee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-20 05:53:25 +0000

    Deploy sunilv1611/jenkinstest to github.com/sunilv1611/jenkinstest.git:gh-pages

---
+++ precache-manifest.3b3c3190a0f8e6630b28e920a827d185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4381834a48fdf40730f0",
+    "url": "/sunilv1611/jenkinstest/static/css/main.10f0b813.chunk.css"
+  },
+  {
+    "revision": "4381834a48fdf40730f0",
+    "url": "/sunilv1611/jenkinstest/static/js/main.4381834a.chunk.js"
+  },
+  {
+    "revision": "ae9829c39adc4c465d95",
+    "url": "/sunilv1611/jenkinstest/static/js/runtime~main.ae9829c3.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/sunilv1611/jenkinstest/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sunilv1611/jenkinstest/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "09d5bf4d133181245992b434121b23f5",
+    "url": "/sunilv1611/jenkinstest/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/sunilv1611/jenkinstest/precache-manifest.3b3c3190a0f8e6630b28e920a827d185.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/sunilv1611/jenkinstest/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
